5578019892f1663ed2233358614de8f4cf0d05fd
Robyn Doyle <robyn.doylee@hotmail.com>
2022-01-04 16:11:50 +0100

    R commit

--- Grupp-1/src/test/java/com/example/Grupp1/Grupp1ApplicationTests.java
+++ Grupp-1/src/test/java/com/example/Grupp1/Grupp1ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.Grupp1;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,12 @@ class Grupp1ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void test(){
+		Joke joke = new Joke(10, "Title", "Family"," HEJ HEJ ROLIGT", "Lwam");
+		Assertions.assertEquals(10, joke.getId());
 	}
 
 }
