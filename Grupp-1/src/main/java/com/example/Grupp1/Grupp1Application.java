c50bf6436ed2d92dcf002959fda23cc30733e29f
Robyn Doyle <robyn.doylee@hotmail.com>
2022-01-04 09:18:25 +0100

    First commit

---
+++ Grupp-1/src/main/java/com/example/Grupp1/Grupp1Application.java
@@ -0,0 +1,13 @@
+package com.example.Grupp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupp1Application.class, args);
+	}
+
+}
---
+++ Grupp-1/src/test/java/com/example/Grupp1/Grupp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Grupp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
