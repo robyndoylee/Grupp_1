c6ef32b9c08b9e1bd8bfa92ad35b3f18dcae3c74
Lwam Ghebresus <lwam.ghebresus@hotmail.com>
2022-01-04 11:00:45 -0800

    form

--- Grupp-1/src/main/java/com/example/Grupp1/Grupp1Controller.java
+++ Grupp-1/src/main/java/com/example/Grupp1/Grupp1Controller.java
@@ -3,13 +3,17 @@ package com.example.Grupp1;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class Grupp1Controller {
 
     @GetMapping("/")
-    String start(Model model){
+    String start(Model model) {
         return "start";
     }
+
+
+    @PostMapping("/")
 
 }
