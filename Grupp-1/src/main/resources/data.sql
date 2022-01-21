a2c6848a2fe792eda36ed48f4b3a7a0f83469a94
niklas sandberg <niklas.sandberg@gmail.com>
2022-01-21 11:08:56 +0100

    version 11:05

--- Grupp-1/src/main/resources/data.sql
+++ Grupp-1/src/main/resources/data.sql
@@ -1,8 +1,20 @@
-INSERT INTO Joke (Title, Category, Body, Author) VALUES ('Test', 'Dark', 'Det här var mörkt', 'Simon');
-INSERT INTO RATING (JokeID, RatingValue) VALUES (1,3);
+INSERT INTO Joke (Title, Category, Body, Author) VALUES ('Alive', 'Dark', 'I''m alive because of two reasons: 1. I was born. 2. I haven''t died yet.', 'Simon');
+INSERT INTO Joke (Title, Category, Body, Author) VALUES ('Stabbed', 'Dark', 'I just read that someone in London gets stabbed every 52 seconds. Poor guy.', 'Simon');
+INSERT INTO Joke (Title, Category, Body, Author) VALUES ('I work with animals', 'Dark', 'I work with animals, the guy says to his date.  "That''s so sweet", she replies. I love a man who cares about animals. Where do you work? "I'' m a butcher, he says', 'Simon');
+INSERT INTO Joke (Title, Category, Body, Author) VALUES ('The hint', 'Adult', 'A woman walks out of the shower, winks at her boyfriend, and says, “Honey, I shaved myself down there. Do you know what that means?” The boyfriend says, “Yeah, it means the drain is clogged again.', 'Simon');
+INSERT INTO Joke (Title, Category, Body, Author) VALUES ('Alive', 'Dark', 'I''m alive because of two reasons: 1. I was born. 2. I haven''t died yet.', 'Simon');
+INSERT INTO JOKE (Title, Category, Body, Author) VALUES ('Alive', 'Dark', 'I''m alive because of two reasons: 1. I was born. 2. I haven''t died yet.', 'Simon');
+INSERT INTO JOKE (Title, Category, Body, Author) VALUES ('Alive', 'Dark', 'I''m alive because of two reasons: 1. I was born. 2. I haven''t died yet.', 'Simon');
+INSERT INTO JOKE (Title, Category, Body, Author) VALUES ('Alive', 'Dark', 'I''m alive because of two reasons: 1. I was born. 2. I haven''t died yet.', 'Simon');
+INSERT INTO JOKE (Title, Category, Body, Author) VALUES ('Alive', 'Dark', 'I''m alive because of two reasons: 1. I was born. 2. I haven''t died yet.', 'Simon');
+INSERT INTO JOKE (Title, Category, Body, Author) VALUES ('Alive', 'Dark', 'I''m alive because of two reasons: 1. I was born. 2. I haven''t died yet.', 'Simon');
+INSERT INTO JOKE (Title, Category, Body, Author) VALUES ('Alive', 'Dark', 'I''m alive because of two reasons: 1. I was born. 2. I haven''t died yet.', 'Simon');
+INSERT INTO RATING (JokeID, RatingValue) VALUES (1,5);
+INSERT INTO RATING (JokeID, RatingValue) VALUES (2,1);
+INSERT INTO RATING (JokeID, RatingValue) VALUES (1,1);
+INSERT INTO RATING (JokeID, RatingValue) VALUES (1,5);
+INSERT INTO RATING (JokeID, RatingValue) VALUES (1,2);
 INSERT INTO RATING (JokeID, RatingValue) VALUES (1,4);
-
-
 
 --INSERT INTO CUSTOMER (FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, PASSWORD) VALUES('Donald', 'Duck', 'donald@disney.com', '123');
 --INSERT INTO CUSTOMER (FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, PASSWORD) VALUES('Daisy', 'Duck', 'daisy@disney.com', 'abc');
@@ -10,3 +22,13 @@ INSERT INTO RATING (JokeID, RatingValue) VALUES (1,4);
 
 --INSERT INTO PURCHASE (BOOK_ID, CUSTOMER_ID, PURCHASE_DATE) VALUES ((SELECT ID FROM BOOK WHERE AUTHOR = 'Douglas Adams'), (SELECT ID FROM CUSTOMER WHERE FIRST_NAME = 'Donald'), '2019-01-07');
 
+--//        jokes.add(new Joke(1, "Alive", "Dark","I'm alive because of two reasons:\n 1. I was born. \n2. I haven't died yet.", "Simon"));
+--//        jokes.add(new Joke(2, "Stabbed", "Dark","I just read that someone in London gets stabbed every 52 seconds. Poor guy.", "Simon"));
+--//        jokes.add(new Joke(3, "I work with animals", "Dark","I work with animals,\" the guy says to his date.  \"That's so sweet,\" she replies. \"I love a man who cares about animals. Where do you work?\"\n\"I'm a butcher,\" he says", "Simon"));
+--//        jokes.add(new Joke(4, "Substitute", "Adult","What does tofu and a dildo have in common? They are both meat substitutes.", "Simon"));
+--//        jokes.add(new Joke(5, "The hint", "Adult","A woman walks out of the shower, winks at her boyfriend, and says, “Honey, I shaved myself down there. Do you know what that means?” The boyfriend says, “Yeah, it means the drain is clogged again.”", "Simon"));
+--//        jokes.add(new Joke(6, "Title", "Family"," HEJ HEJ ROLIGT", "Lwam"));
+--//        jokes.add(new Joke(7, "Title", "Family"," HEJ HEJ ROLIGT", "Lwam"));
+--//        jokes.add(new Joke(8, "Title", "Family"," HEJ HEJ ROLIGT", "Lwam"));
+--//        jokes.add(new Joke(9, "Title", "Family"," HEJ HEJ ROLIGT", "Lwam"));
+--//        jokes.add(new Joke(10, "Title", "Family"," HEJ HEJ ROLIGT", "Lwam"));
